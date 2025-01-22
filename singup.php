f6597146c63de576ae79873587f4eff8699992b1
visunu732 <82407527+visunu732@users.noreply.github.com>
2025-01-22 11:40:41 +0530

    Add files via upload

---
+++ singup.php
@@ -0,0 +1,43 @@
+<?php
+
+$host="localhost";
+$username="root";
+$password="";
+$dbname="singpup";
+
+$conn=mysqli_connect($host,$username,$password,$dbname);
+
+
+if(!$conn){
+   echo"not connect";
+
+}
+else{
+    echo"connect";
+}
+?>
+
+
+
+//  <h2 class="slider-title">SHOPPING NOW</h2>
+                   
+                   <div class="product-content">              
+                        <h3 class="woocommerce-loop-product__title">SS VESSELS KOODAI</h3></a>
+                    <span class="price">MRP
+                  <ins>  <span>&#8377;</span>752.00</span></ins></span>
+                </div>
+            </div>
+                                    <div class="buttons_added">
+                                        <h3 classs="add-qnt">ADD QUANTITY</h3>
+                                        <button type="button" value="-" class="minus">-</button>
+                                        <input type="number" id="quantity_665e0a0261d0e" class="input-text qty text" step="1" min="1" max=""value="1"inputmode="numeric">
+                                        <button type="button" value="+" class="plus">+</button>
+                                    </div><br>
+                                    <button class="button-buy">BUY NOW!</button>
+                                  <ul>
+                                    <li>stains steel tumbler ambesh</li>
+                                    <li>weight :200gm</li>
+                                    <li>color :steel body</li>
+                                  </ul>
+                                
+                        </div>
